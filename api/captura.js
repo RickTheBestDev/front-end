42a75e5fceebda85668280d968678036108a94bf
RickTheBestDev <rickps4slim@gmail.com>
2026-05-11 16:02:50 -0300

    Adicionando design responsivo para diferentes tipos de tela.

--- api/captura.js
+++ api/captura.js
@@ -1,64 +1,46 @@
-const url= "https://jsonplaceholder.typicode.com/users";
-
-fetch(url).then(resposta => { 
-    if (!resposta.ok){
-        throw new Error("Erro na rede: " + resposta.status)
-    }
-    return resposta.json();
-})
-.then(dados => {
-    console.log("Dados recebidos: \n", dados )
-})
-.catch( erro => console.error("Houve um probelma com a requisição: " + erro))
-
-
-
-const video = document.getElementById("cameraFeed");
-const canvas = document.getElementById("capturedImage");
+const video = document.getElementById("cameraFeed")
+const canvas = document.getElementById("capturedImage")
 const botao = document.getElementById("captureButton");
 const contexto = canvas.getContext("2d");
 
-//solicitar acesso a câmera
+// Solicitar permissão para acessar a câmera 
 navigator.mediaDevices.getUserMedia({ video: true })
     .then(stream => {
         video.srcObject = stream;
-        video.play(); 
+        video.play();
     })
-    .catch( erro => {
-        console.error("erro ao acessar a câmera " + erro);
+    .catch(erro => {
+        console.error("Erro ao acessar a câmera: ", erro);
     });
 
-//atribuir uma função para o botão de captura
+//atribuir uma função ao botão de captura
 
 botao.addEventListener("click", () => {
-    //Desenhar o quadro atual do vídeo na área do canvas
-    contexto.drawImage(video, 0, 0, canvas.width = 1000, canvas.height = 1000);
+    // Desenhar o quadro atual do vídeo na area do canvas
+    contexto.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    //obter a imagem capturada como um data URL
-    const imagemDataURL = canvas.toDataURL("image/png");
-    //enviar a imagem para o servidor 
-    enviarImagemParaServidor(imagemDataURL);
+    const imageDataURL = canvas.toDataURL("image/png");
+    enviarImagemParaServidor(imageDataURL);
+});
 
-})
-
-//função para enviar a imagem para o servidor
-function enviarImagemParaServidor(imagemDataURL) {
-    //simular o envio da imagem para o servidor
+function enviarImagemParaServidor(imageDataURL) {
+    // Implementar a lógica para enviar a imagem para o servidor
     console.log("Enviando imagem para o servidor...");
-    
-    fetch("/", {
+
+    const base64String = imageDataURL.split(",")[1];
+
+    fetch("http://DOP3080-1247456:8000/images", {
         method: "POST",
-        body: JSON.stringify({ imagem: imagemDataURL }),
+        body: JSON.stringify({ image: base64String, mime_type: 'image/png' }),
         headers: {
             "Content-Type": "application/json"
         }
-})
-.then( resposta => resposta.json())
-.then( dados => {
-    console.log("Resposta do servidor: ", dados);
-})
-
-.catch( erro => {
-    console.error("Erro ao enviar a imagem para o servidor: " + erro);
-})
+    })
+        .then(response => response.json())
+        .then(dados => {
+        console.log("Resposta do servidor: ", dados);
+    })
+        .catch(erro => {
+        console.error("Erro ao enviar a imagem: ", erro);
+        })
 }
